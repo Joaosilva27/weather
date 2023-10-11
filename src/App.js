eb1bf704f9453bb6c6e3913c7d6afba83a28c281
Joaosilva27 <joaosilva7875@gmail.com>
2023-10-11 21:30:26 +0200

    default page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import axios from "axios";
 import SearchIcon from "./images/search.png";
 import cloudyWeatherIcon from "./images/cloudy.png";
 import cloudyAndSunnyIcon from "./images/cloudAndsunny.png";
-import rainyWeatherIcon from "./images/rainy.jpg";
+import rainyWeatherIcon from "./images/rainy.png";
 import sunnyWeatherIcon from "./images/sunny.png";
 import locationIcon from "./images/location.png";
 
@@ -36,6 +36,7 @@ const SearchBar = () => {
   const [currentSnow, setCurrentSnow] = useState("");
   const [currentWindSpeed, setCurrentWindSpeed] = useState("");
   const [weatherIcon, setWeatherIcon] = useState("");
+  const [isNoCitySearched, setIsNoCitySearched] = useState(true);
 
   const onSearchCity = e => {
     e.preventDefault();
@@ -57,10 +58,8 @@ const SearchBar = () => {
 
         const localTime = new Date(utcTime.getTime() + timezoneOffsetSeconds * 1000);
 
-        // Adjust the time by subtracting 2 hours (7200 seconds)
         const adjustedLocalTime = new Date(localTime.getTime() - 7200 * 1000);
 
-        // Format the adjusted local time as a string
         const formattedTime = adjustedLocalTime.toLocaleTimeString([], {
           hour: "2-digit",
           minute: "2-digit",
@@ -69,7 +68,6 @@ const SearchBar = () => {
         const sunriseTimestamp = response.data.sys.sunrise * 1000;
         const localSunriseTime = new Date(sunriseTimestamp + timezoneOffsetSeconds * 1000);
 
-        // Adjust sunrise by subtracting 2 hours
         localSunriseTime.setTime(localSunriseTime.getTime() - 7200 * 1000);
 
         const formattedTimeSunrise = localSunriseTime.toLocaleTimeString([], {
@@ -80,7 +78,7 @@ const SearchBar = () => {
         const sunsetTimestamp = response.data.sys.sunset * 1000;
         const localSunsetTime = new Date(sunsetTimestamp + timezoneOffsetSeconds * 1000);
 
-        // Adjust sunset by subtracting 2 hours
+        // Adjust sunset by subtracting 2 hours (converting it to local time)
         localSunsetTime.setTime(localSunsetTime.getTime() - 7200 * 1000);
 
         const formattedTimeSunset = localSunsetTime.toLocaleTimeString([], {
@@ -97,33 +95,25 @@ const SearchBar = () => {
         setCurrentSnow(response.data.snow);
         setCurrentWindSpeed(response.data.wind.speed);
         setCurrentHumidity(response.data.main.humidity);
+        setIsNoCitySearched(false);
 
-        // Calculate the length of the day in milliseconds
         const lengthOfDay = sunsetTimestamp - sunriseTimestamp;
 
-        // Calculate the current daylight time in milliseconds since sunrise
         const now = new Date().getTime();
         const remainingDaylight = sunsetTimestamp - now;
 
-        // Convert milliseconds to hours and minutes
         const lengthOfDayHours = Math.floor(lengthOfDay / (60 * 60 * 1000));
         const lengthOfDayMinutes = Math.floor((lengthOfDay % (60 * 60 * 1000)) / (60 * 1000));
 
         const remainingDaylightHours = Math.floor(remainingDaylight / (60 * 60 * 1000));
         const remainingDaylightMinutes = Math.floor(remainingDaylight % (60 * 60 * 1000)) / (60 * 1000);
 
-        // Round the values to the nearest whole number
         const totalRemainingMinutes = Math.round(remainingDaylightHours * 60 + remainingDaylightMinutes);
         const remainingHours = Math.floor(totalRemainingMinutes / 60);
         const remainingMinutes = totalRemainingMinutes % 60;
 
         setCurrentDaylight(`${remainingHours > 0 ? remainingHours + "h" : ""} ${remainingMinutes > 0 ? remainingMinutes + "m" : "none"}`);
         setCurrentLengthOfDay(`${lengthOfDayHours > 0 ? lengthOfDayHours + "h" : ""} ${lengthOfDayMinutes > 0 ? lengthOfDayMinutes + "m" : ""}`);
-
-        console.log("hi");
-        console.log(formattedTimeSunset);
-        console.log(currentTimezone);
-        console.log(formattedTimeSunset - currentTimezone);
 
         if (response.data.weather[0].main === "Clear") {
           setWeatherIcon(sunnyWeatherIcon);
@@ -142,57 +132,6 @@ const SearchBar = () => {
 
     setSearchCity("");
   };
-
-  // Fetch weather data for Amsterdam only when the component mounts
-  useEffect(() => {
-    if (!searchCity) {
-      axios
-        .get(apiUrlWeather, {
-          params: {
-            q: "Amsterdam", // Fetch data for Amsterdam by default
-            appid: apiKeyWeather,
-            units: "metric",
-          },
-        })
-        .then(response => {
-          console.log(response.data);
-
-          // Convert the Unix timestamp to milliseconds and add the timezone offset
-          const timestamp = (response.data.dt + response.data.timezone) * 1000;
-
-          // Create a Date object from the adjusted timestamp
-          const localTime = new Date(timestamp);
-
-          // Format the local time as a string (adjust the options as needed)
-          const formattedTime = localTime.toLocaleTimeString([], {
-            hour: "2-digit",
-            minute: "2-digit",
-          });
-
-          setCurrentCity(response.data.name);
-          setCurrentWeather(Math.round(response.data.main.temp));
-          setCurrentTimezone(formattedTime);
-          setCurrentRain(response.data.rain);
-          setCurrentSnow(response.data.snow);
-          setCurrentWindSpeed(response.data.wind.speed);
-          setCurrentHumidity(response.data.main.humidity);
-
-          if (response.data.weather[0].main === "Clear") {
-            setWeatherIcon(sunnyWeatherIcon);
-          } else if (response.data.weather[0].main === "Rain") {
-            setWeatherIcon(rainyWeatherIcon);
-          } else if (response.data.weather[0].main === "Clouds") {
-            setWeatherIcon(cloudyWeatherIcon);
-          } else {
-            setWeatherIcon(cloudyAndSunnyIcon);
-          }
-        })
-        .catch(error => {
-          console.error(error);
-          setWeatherIcon("");
-        });
-    }
-  }, []);
 
   useEffect(() => {
     setSunsetData([
@@ -249,90 +188,105 @@ const SearchBar = () => {
 
   return (
     <div>
-      <div style={{ backgroundColor: "#FAFAFA" }} className='h-12 w-full rounded-xl flex items-center justify-center'>
-        <form onSubmit={onSearchCity} className='h-12 w-full rounded-xl flex items-center justify-center'>
-          <input
-            value={searchCity}
-            onChange={e => setSearchCity(e.target.value)}
-            placeholder='Search Location'
-            style={{ backgroundColor: "#FAFAFA", outline: "none" }}
-            className='h-12 w-full rounded-xl pl-4'
-          />
-          <button type='submit' style={{ backgroundColor: "#FAFAFA", border: "none" }} className='h-12'>
-            <img onClick={onSearchCity} className='h-4.5 mr-3' src={SearchIcon} alt='Search Icon' />
-          </button>
-        </form>
-      </div>
-      <div className='mt-8 flex justify-center flex-col items-center'>
-        <div>
-          <img className='h-36' src={weatherIcon} alt='Weather Icon' />
-        </div>
-        <div className='flex justify-center items-center'>
-          <h1 className={`font-semibold ${getCityNameFontSizeClass()}`}>{currentCity}</h1>
-          <img className='h-5.5 ml-3' src={locationIcon} alt='Location Icon' />
-        </div>
-        <div className='flex mt-4 mb-4'>
-          <p className='font-semibold text-7xl'>{currentWeather}</p>
-          {currentCity && <span className='text-2xl ml-2 mb-2'>º</span>}
-        </div>
-      </div>
       <div>
-        <div style={{ backgroundColor: "#FAFAFA" }} className='flex w-full justify-between rounded-xl pt-2 pb-2 pl-5 pr-5'>
-          <div className='flex flex-col justify-center items-center'>
-            <span style={{ color: "#C4C4C4" }} className='text-sm mb-0.5'>
-              TIME
-            </span>
-            <span style={{ color: "#9A9A9A" }} className='font-medium'>
-              {currentTimezone}
-            </span>
-          </div>
-          <div className='flex flex-col justify-center items-center'>
-            <span style={{ color: "#C4C4C4" }} className='text-sm mb-0.5'>
-              HUMIDITY
-            </span>
-            <span style={{ color: "#9A9A9A" }} className='font-medium'>
-              {currentHumidity}
-            </span>
-          </div>
-          <div className='flex flex-col justify-center items-center'>
-            <span style={{ color: "#C4C4C4" }} className='text-sm mb-0.5'>
-              {currentSnow ? <p>% SNOW</p> : <p>% RAIN</p>}
-            </span>
-            <span style={{ color: "#9A9A9A" }} className='font-medium'>
-              {currentSnow ? <p>{currentSnow}</p> : currentRain ? <p>{currentRain}</p> : <p>0</p>}
-            </span>
-          </div>
-          <div className='flex flex-col justify-center items-center'>
-            <span style={{ color: "#C4C4C4" }} className='text-sm mb-0.5'>
-              WIND
-            </span>
-            <span style={{ color: "#9A9A9A" }} className='font-medium'>
-              {currentWindSpeed} /h
-            </span>
-          </div>
+        <div style={{ backgroundColor: "#FAFAFA" }} className='h-12 w-full rounded-xl flex items-center justify-center'>
+          <form onSubmit={onSearchCity} className='h-12 w-full rounded-xl flex items-center justify-center'>
+            <input
+              value={searchCity}
+              onChange={e => setSearchCity(e.target.value)}
+              placeholder='Search Location'
+              style={{ backgroundColor: "#FAFAFA", outline: "none" }}
+              className='h-12 w-full rounded-xl pl-4'
+            />
+            <button type='submit' style={{ backgroundColor: "#FAFAFA", border: "none" }} className='h-12'>
+              <img onClick={onSearchCity} className='h-4.5 mr-3' src={SearchIcon} alt='Search Icon' />
+            </button>
+          </form>
         </div>
-        <div style={{ backgroundColor: "#FAFAFA" }} className='flex flex-col w-full justify-between rounded-xl mt-4 pt-2 pb-2 pl-5 pr-5'>
-          <span style={{ color: "#C4C4C4" }} className='text-sm mb-8'>
-            SUNRISE & SUNSET
-          </span>
-          <div className='w-full flex justify-between items-center pl-5 pr-3 '>
+
+        {isNoCitySearched ? (
+          <div style={{ height: "80vh" }} className='flex justify-center items-center'>
+            <img className='h-20 rotate' src={sunnyWeatherIcon} />
             <span style={{ color: "#9A9A9A" }} className='font-medium'>
-              {currentSunrise}
-            </span>
-            <span style={{ color: "#9A9A9A" }} className='font-medium'>
-              {currentSunset}
+              Please type in the name of a city <span className='animated-dots'></span>
             </span>
           </div>
-          <SunriseAndSunsetGraph sunsetData={sunsetData} />
-          <div className='mt-3 flex flex-col'>
-            <span className='font-medium text-sm'>
-              <span style={{ color: "#9A9A9A" }}>Length of day:</span> {currentLengthOfDay}
-            </span>
-            <span className='font-medium text-sm mt-1'>
-              <span style={{ color: "#9A9A9A" }}>Remaining daylight:</span> {currentDaylight}
-            </span>
+        ) : (
+          <div>
+            <div className='mt-8 flex justify-center flex-col items-center'>
+              <div>
+                <img className='h-36' src={weatherIcon} alt='Weather Icon' />
+              </div>
+              <div className='flex justify-center items-center'>
+                <h1 className={`font-semibold ${getCityNameFontSizeClass()}`}>{currentCity}</h1>
+                <img className='h-5.5 ml-3' src={locationIcon} alt='Location Icon' />
+              </div>
+              <div className='flex mt-4 mb-4'>
+                <p className='font-semibold text-7xl'>{currentWeather}</p>
+                {currentCity && <span className='text-2xl ml-2 mb-2'>º</span>}
+              </div>
+            </div>
+            <div>
+              <div style={{ backgroundColor: "#FAFAFA" }} className='flex w-full justify-between rounded-xl pt-2 pb-2 pl-5 pr-5'>
+                <div className='flex flex-col justify-center items-center'>
+                  <span style={{ color: "#C4C4C4" }} className='text-sm mb-0.5'>
+                    TIME
+                  </span>
+                  <span style={{ color: "#9A9A9A" }} className='font-medium'>
+                    {currentTimezone}
+                  </span>
+                </div>
+                <div className='flex flex-col justify-center items-center'>
+                  <span style={{ color: "#C4C4C4" }} className='text-sm mb-0.5'>
+                    HUMIDITY
+                  </span>
+                  <span style={{ color: "#9A9A9A" }} className='font-medium'>
+                    {currentHumidity}
+                  </span>
+                </div>
+                <div className='flex flex-col justify-center items-center'>
+                  <span style={{ color: "#C4C4C4" }} className='text-sm mb-0.5'>
+                    {currentSnow ? <p>% SNOW</p> : <p>% RAIN</p>}
+                  </span>
+                  <span style={{ color: "#9A9A9A" }} className='font-medium'>
+                    {currentSnow ? <p>{currentSnow}</p> : currentRain ? <p>{currentRain}</p> : <p>0</p>}
+                  </span>
+                </div>
+                <div className='flex flex-col justify-center items-center'>
+                  <span style={{ color: "#C4C4C4" }} className='text-sm mb-0.5'>
+                    WIND
+                  </span>
+                  <span style={{ color: "#9A9A9A" }} className='font-medium'>
+                    {currentWindSpeed} /h
+                  </span>
+                </div>
+              </div>
+              <div style={{ backgroundColor: "#FAFAFA" }} className='flex flex-col w-full justify-between rounded-xl mt-4 pt-2 pb-2 pl-5 pr-5'>
+                <span style={{ color: "#C4C4C4" }} className='text-sm mb-8'>
+                  SUNRISE & SUNSET
+                </span>
+                <div className='w-full flex justify-between items-center pl-5 pr-3.5 '>
+                  <span style={{ color: "#9A9A9A" }} className='font-medium'>
+                    {currentSunrise}
+                  </span>
+                  <img src={weatherIcon} className='h-4 relative top-5 right-2 z-10' />
+                  <span style={{ color: "#9A9A9A" }} className='font-medium'>
+                    {currentSunset}
+                  </span>
+                </div>
+                <SunriseAndSunsetGraph sunsetData={sunsetData} />
+                <div className='mt-3 flex flex-col'>
+                  <span className='font-medium text-sm'>
+                    <span style={{ color: "#9A9A9A" }}>Length of day:</span> {currentLengthOfDay}
+                  </span>
+                  <span className='font-medium text-sm mt-1'>
+                    <span style={{ color: "#9A9A9A" }}>Remaining daylight:</span> {currentDaylight}
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
